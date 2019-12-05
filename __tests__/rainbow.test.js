b3467e35a92b30a1a0768acc747ff4da13e4bc8d
Brian Emilius <BrianEmilius@users.noreply.github.com>
2019-12-05 10:53:14 +0100

    Delete rainbow.test.js

--- __tests__/rainbow.test.js
+++
@@ -1,7 +0,0 @@
-let rainbow = require("../index")
-
-describe("The function rainbow-log", function() {
-	test("should return undefined", function() {
-		expect(rainbow("Hello, World!")).toBeUndefined()
-	})
-})
